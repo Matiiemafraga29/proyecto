b015289a6bd0e74f2bd1a18b7fa065ab4f4ae935
Matii Fraga <matiifraga29@hotmail.com>
2022-09-20 20:30:28 -0300

    prueba
    prueba

--- proyecto_pp2.1/abmprodu.php
+++ proyecto_pp2.1/abmprodu.php
@@ -1,55 +1,25 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-  <!-- Bootstrap -->
-<link rel ="stylesheet" href="css/bootstrap.min.css"/>
-<!-- Estilos en css -->
-<link rel="stylesheet"  href="css/estilo.css"/>
-  </head>
-  <body class="m=0 row justify-content-center text-center">
-
-
-<!-- Barra de navegador boostrap-->
-<nav class="navbar navbar-expand-md navbar-light navbar-dark bg-dark border-3 border-bottom border-primary">
-  <div class="container-fluid">
-
-   <a class="navbar-brand" href="index.php"> Speed Data</a>
-
-      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#MenuNavegacion">
-              <span class="navbar-toggler-icon"></span>
-      </button>
-  </div>
-
-   <div id="MenuNavegacion" class="collapse navbar-collapse show ">
-      <ul class="navbar-nav ms-3">
-        <li class="nav-item active"><a class="nav-link" href="produ.html">Produccion</a></li>
-        <li class="nav-item"><a class="nav-link" href="stock.html">stock</a></li>
-        <li class="nav-item"><a class="nav-link" href="imp_etiq.html">Etiquetas</a></li>  
-        <li class="nav-item"><a class="nav-link" href="recetas.html">Recetas</a></li>
-        <li class="nav-item"><a href="login.php" class="nav-link" >Cerrar Sesión</a></li>
-      </ul>
-   </div>
-</nav>
-
+    <?php include("header.php");?>
     <?php include("conexion.php") ?>
     <?php 
     if($_POST){
-        $objconexion = new conexion();
-        $cantBols=$_POST['cantidad'];
-        $cod_sap=$_POST['material'];
-        $peso=$_POST['peso'];
+        if(ctype_digit($_POST['cantidad']) && ctype_digit($_POST['peso'])){
+            $objconexion = new conexion();
+            $cantBols=$_POST['cantidad'];
+            $cod_sap=$_POST['material'];
+            $peso=$_POST['peso'];
 
-        $insertar="INSERT INTO `lotes` (`lote_id`, `cantidad_bolsones`, `cod_sap`, `fecha`) VALUES (NULL, '$cantBols', '$cod_sap', current_timestamp())";
-        $objconexion->ejecutar($insertar);
-
-        $resultado=$objconexion->consultar("SELECT * FROM `lotes` ORDER BY `lotes`.`lote_id` ASC");
-        $ultimoLote=end($resultado)['0'];
-        $insertar="INSERT INTO `bolsas` (`bolsa_id`, `peso`, `lote_id`, `estatus`) VALUES (NULL, '$peso', '$ultimoLote', 'Deposito')";
-        for($i=0;$i<$cantBols;$i++){
+            $insertar="INSERT INTO `lotes` (`lote_id`, `cantidad_bolsones`, `cod_sap`, `fecha`) VALUES (NULL, '$cantBols', '$cod_sap', current_timestamp())";
             $objconexion->ejecutar($insertar);
-        }
 
+            $resultado = $objconexion->consultar("SELECT * FROM `lotes` ORDER BY `lotes`.`lote_id` ASC");
+            $ultimoLote = end($resultado)['0'];
+            $insertar = "INSERT INTO `bolsas` (`bolsa_id`, `peso`, `lote_id`, `estatus`) VALUES (NULL, '$peso', '$ultimoLote', 'Deposito')";
+            for ($i=0; $i<$cantBols ;$i++){
+            $objconexion->ejecutar($insertar);
+            }
+        }else{
+            $valid = false;
+        }
     }
     
 
@@ -57,15 +27,15 @@
 
 <!-- Formulario de ejemplo-->
 
-		<h1>MODIFICACION Y CONSULTA DE STOCK</h1>
+		<h1>INGRESOD DE MATERIA PRIMA</h1>
 
         <div class="container px-5 my-5">
-    <form id="contactForm" data-sb-form-api-token="API_TOKEN" action="abmprodu.php" metod="POST">
+    <form id="contactForm" data-sb-form-api-token="API_TOKEN" action="abmprodu.php" method="post">
         
         
         <div class="form-floating mb-3">
-            <input class="form-control" id="cantidad" type="text" placeholder="Cantidad" data-sb-validations="required" name="cantidad"/>
-            <label for="cantidad">Cantidad</label>
+            <input class="form-control" id="cantidad" type="number" placeholder="Cantidad" data-sb-validations="required" name="cantidad"/>
+            <label for="cantidad">Cantidad de bolsas</label>
             <div class="invalid-feedback" data-sb-feedback="cantidad:required">Cantidad is required.</div>
         </div>
         <div class="form-floating mb-3">
@@ -88,16 +58,10 @@
             <label for="material">Material</label>
         </div>
         <div class="form-floating mb-3">
-            <input class="form-control" id="peso" type="text" placeholder="Peso" data-sb-validations="required" name="peso"/>
-            <label for="peso">Peso</label>
+            <input class="form-control" id="peso" type="number" placeholder="Peso" data-sb-validations="required" name="peso"/>
+            <label for="peso">Peso /kg</label>
             <div class="invalid-feedback" data-sb-feedback="peso:required">Peso is required.</div>
         </div>
-        <!--<div class="mb-3">
-            <div class="form-check form-switch">
-                <input class="form-check-input" id="aprobado" type="checkbox" name="aprobado" />
-                <label class="form-check-label" for="aprobado">Aprobado</label>
-            </div>
-        </div>-->
         <div class="d-none" id="submitSuccessMessage">
             <div class="text-center mb-3">
                 <div class="fw-bolder">Form submission successful!</div>
@@ -105,11 +69,11 @@
                 <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
             </div>
         </div>
-        <div class="d-none" id="submitErrorMessage">
+        <div class="d-none" id="">
             <div class="text-center text-danger mb-3">Error sending message!</div>
         </div>
         <div class="d-grid">
-            <imput class="btn btn-primary btn-lg disabled" id="submitButton" type="submit" value="Submit">Submit</imput>
+            <button class="btn btn-primary btn-lg" id="submit" type="submit" value="Submit">GUARDAR</button>
         </div>
     </form>
 </div>
--- proyecto_pp2.1/index.php
+++ proyecto_pp2.1/index.php
@@ -4,56 +4,75 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
 <title>Menu</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Bootstrap 
+   <!-- Bootstrap 
 <link rel ="stylesheet" href="css/bootstrap.min.css"/>-->
 <!-- Estilos en css -->
-<link rel="stylesheet"  href="css/estilo.css"/>
+<link rel="stylesheet"  href="css/style.css"/>
 </head>
+<!-- Js Hambur -->
+<script src="https://kit.fontawesome.com/9f74c55650.js" crossorigin="anonymous">
+
+</script>
+
+<script defer src="js/index.js"></script>
+
 
 <body class="row justify-content-center text-center">
 
 <!--<nav><h1>Proyecto PP2  | Speed Data</h1></nav>-->
 
-<!-- Barra de navegador boostrap -->
-<nav class="navbar navbar-expand-md navbar-light navbar-dark bg-dark border-3 border-bottom border-primary">
-            <div class="container-fluid">
 
-             <a class="navbar-brand" href="index.php"> Speed Data</a>
+<!--Barra de navegacion hambur media queries -->
 
-                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#MenuNavegacion">
-                        <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
+<header class="header">  
+<nav class="nav"> 
+    <a href="index.php" class="logo nav-link ">Speed Data</a>
+<button class="nav-toggle">
+    <i class="fa-solid fa-bars"></i>
+</button>
+    <ul class="nav-menu ">
 
-			       <div id="MenuNavegacion" class="collapse navbar-collapse show ">
-                <ul class="navbar-nav ms-3">
-                   <li class="nav-item active"><a href="login.php" class="nav-link" >Cerrar Sesión</a></li>
-                </ul>
-			       </div>
+    <li class="nav-menu-item">
+            <a href="produ.php" class="nav-menu-link nav-link ">produccion</a>
+        </li>
+    <li class="nav-menu-item">
+            <a href="stock.html" class="nav-menu-link nav-link ">Stock</a>
+        </li>
+    <li class="nav-menu-item">
+            <a href="imp_etiq.html" class="nav-menu-link nav-link ">Imp. de etiquetas</a>
+        </li>
+    <li class="nav-menu-item">
+            <a href="recetas.html" class="nav-menu-link nav-link ">Recetas</a>
+        </li>
+        <li class="nav-menu-item">
+            <a href="login.php" class="nav-menu-link nav-link ">Cerrar sesion</a>
+        </li>
+        </li>
+    
+</ul>
 </nav>
+</header>  
+
+
+
+
+
+
+
+
+<!-- Barra de navegador boostrap -->
+
 
 
 <!-- MENU -->
-        <div class="centrado">
-                                          
-<br>
 
-
-       <div class="btn-group-vertical "  >                                   <!--Color borde|tipo de boton|alineacion|tamaño -->
-       <a href="produ.html " button type="button" class="border-dark rounded btn btn-primary btn-block  btn-lg  mb-3">PRODUCCION</button></a>
-        <a href="stock.html " button type="button" class="border-dark rounded btn btn-primary btn-block  btn-lg mb-3">STOCK</button></a>
-        <a href="imp_etiq.html " button type="button" class="border-dark rounded btn btn-primary btn-block  btn-lg mb-3">IMP. DE ETIQUETAS</button></a>
-        <a href="recetas.html " button type="button" class="border-dark rounded btn btn-primary btn-block  btn-lg mb-3">RECETAS</button></a>
-    </div>
-</div>
-
+  
     
 <!-- Footer acomodor logo falta -->
 <footer>
-     &copy; 2022
-     <img src="img/logo.png" width="80"
-       height="60">
+
+     &copy; 2022 
+     <img src="img/logo.png" width="100" height="60">
 
  </footer>
 </body>
